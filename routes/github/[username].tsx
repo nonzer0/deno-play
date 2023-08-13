9a1e2e0eeef0f71aa0057e636a9cd74bb300f6d0
Neil West <nswest23@gmail.com>
2023-08-12 20:30:26 -0400

    initial commit

---
+++ routes/github/[username].tsx
@@ -0,0 +1,24 @@
+import { RouteContext } from "$fresh/server.ts";
+
+interface GithubResponse {
+    login: string;
+    name: string;
+    avatar_url: string;
+}
+
+export default async function Page(_req: Request, ctx: RouteContext) {
+    const resp = await fetch(`https://api.github.com/users/${ctx.params.username}`,)
+    if(!resp.ok) {
+        return <h1>An error occurred</h1>
+    }
+
+    const { login, name, avatar_url} = await resp.json() as GithubResponse;
+
+    return (
+        <div>
+            <img src={avatar_url}  width={64} height={64}/>
+            <h1>{name}</h1>
+            <p>{login}</p>
+        </div>
+    )
+}
